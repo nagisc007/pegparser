a8daa555fef35ff8db1b45a3d308022066614f09
nagisc007 <nagisc007@yahoo.co.jp>
2017-12-08 19:15:47 +0900

    [WIP] add setup.py file

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017 N.T.WORKS Authors. All Rights Reserved.
+#
+#      http://opensource.org/licenses/mit-license.php
+#
+
+"""File Description
+"""
+
+import sys
+
+def main(argv):
+    # short description
+    return 0
+
+if __name__ == '__main__':
+    main(sys.argv)
